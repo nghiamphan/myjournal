51cd317cd0d34a92d6921ccd778be8ca9dbeb5e8
Nghia <19204686+nghiamphan@users.noreply.github.com>
2024-02-27 16:40:33 -0400

    add auto build and deploy scripts

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,48 +1,49 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true,
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true,
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended"
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
 	],
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
 		},
-		"ecmaVersion": 2018,
-		"sourceType": "module"
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
 	},
-	"plugins": [
-		"react",
+	'plugins': [
+		'react',
 	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
 		],
-		"linebreak-style": [
-			"error",
-			"windows"
+		'linebreak-style': [
+			'error',
+			'unix'
 		],
-		"quotes": [
-			"error",
-			"single"
+		'quotes': [
+			'error',
+			'single'
 		],
-		"semi": [
-			"error",
-			"never"
+		'semi': [
+			'error',
+			'never'
 		],
-		"eqeqeq": "error",
-		"no-trailing-spaces": "error",
-		"object-curly-spacing": [
-			"error", "always"
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
 		],
-		"arrow-spacing": [
-			"error", { "before": true, "after": true }
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
 		],
-		"no-console": 0,
-		"react/prop-types": 0
+		'no-console': 0,
+		'react/prop-types': 0
 	}
 }
